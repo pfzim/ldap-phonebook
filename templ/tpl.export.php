9fa7c3720d6d78489ab6c17730982242656db706
Dmitry V. Zimin <pfzim@mail.ru>
2017-04-11 15:34:32 +0300

    Add export for Windows PhoneBook client (pb.php?action=export)

--- templ/tpl.export.php
+++ templ/tpl.export.php
@@ -1,11 +1,16 @@
-<?php if(!defined("Z_PROTECTED")) exit; ?>
-<!XML>
+<?php if(!defined("Z_PROTECTED")) exit; ?><?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <phonebook>
-	<?php $i = 0; if($db->data !== FALSE) foreach($db->data as $row) { $i++; ?>
+<?php $i = 0; if($db->data !== FALSE) foreach($db->data as $row) { $i++; ?>
 	<contact>
-		<name><?php eh($row[0]); ?></name>
-		<phone1><?php eh($row[1]); ?></phone1>
-		<phone2><?php eh($row[2]); ?></phone2>
+		<name><?php eh($row[3].' '.$row[2]); ?></name>
+		<phone1><?php eh($row[7]); ?></phone1>
+		<phone2><?php eh($row[8]); ?></phone2>
+		<phone3></phone3>
+		<mail><?php eh($row[9]); ?></mail>
+		<organisation><?php eh($row[5]); ?></organisation>
+		<departament><?php eh($row[4]); ?></departament>
+		<section></section>
+		<position><?php eh($row[6]); ?></position>
 	</contact>
-	<?php } ?>
+<?php } ?>
 </phonebook>
