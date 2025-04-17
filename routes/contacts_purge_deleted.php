bb5268bae019040a0bb90a46f5844507b154ceea
Dmitry V. Zimin <pfzim@mail.ru>
2025-04-17 08:01:26 +0300

    Deleting photos when purging contacts

--- routes/contacts_purge_deleted.php
+++ routes/contacts_purge_deleted.php
@@ -4,7 +4,21 @@ function contacts_purge_deleted(&$core, $params, $post_data)
 {
 	assert_permission_ajax(0, PB_ACCESS_ADMIN);	// level 0 having Write access mean admin
 
-	$core->db->put(rpv('DELETE FROM `@contacts` WHERE (`flags` & ({%PB_CONTACT_AD_DELETED}))'));
+	$i = 0;
 
-	echo '{"code": 0, "message": "Deleted contacts"}';
+	$core->db->select_assoc_ex($result, rpv('SELECT c.`id` FROM `@contacts` AS c WHERE (c.`flags` & ({%PB_CONTACT_AD_DELETED}))'));
+	foreach($result as &$row)
+	{
+		$filename = ROOT_DIR.'photos'.DIRECTORY_SEPARATOR.'t'.$row['id'].'.jpg';
+
+		if(file_exists($filename))
+		{
+			unlink($filename);
+		}
+
+		$core->db->put(rpv('DELETE FROM `@contacts` WHERE `id` = #', $row['id']));
+		$i++;
+	}
+
+	echo '{"code": 0, "message": "Deleted '.$i.' contacts"}';
 }
